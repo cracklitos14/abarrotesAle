56c767756327bd7fe4fffa5a161631db7f0d5d12
Carlos S <sanchezvargascarlos1@gmail.com>
2026-03-05 20:58:03 -0600

    cambio api infinityfree

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,20 +2,20 @@ export const environment = {
   production: true,
 
   // Auth (login)
-  authUrl: 'https://abarrotesale.shop/backend-ale/index.php?endpoint=login',
+  authUrl: 'https://abarrotesale.infinityfreeapp.com/backend-ale/index.php?endpoint=login',
 
   // API base
-  apiUrl: 'https://abarrotesale.shop/backend-ale/api',
+  apiUrl: 'https://abarrotesale.infinityfreeapp.com/backend-ale/api',
 
   // Dashboard
-  dashboardUrl: 'https://abarrotesale.shop/backend-ale/api/dashboard.php',
+  dashboardUrl: 'https://abarrotesale.infinityfreeapp.com/backend-ale/api/dashboard.php',
 
   // Productos
-  productsUrl: 'https://abarrotesale.shop/backend-ale/api/products.php',
-  createUrl:   'https://abarrotesale.shop/backend-ale/api/guardar_producto.php',
-  updateUrl:   'https://abarrotesale.shop/backend-ale/api/editar_producto.php',
-  deleteUrl:   'https://abarrotesale.shop/backend-ale/api/eliminar_producto.php',
+  productsUrl: 'https://abarrotesale.infinityfreeapp.com/backend-ale/api/products.php',
+  createUrl:   'https://abarrotesale.infinityfreeapp.com/backend-ale/api/guardar_producto.php',
+  updateUrl:   'https://TU-SUBDOMINIO.infinityfreeapp.com/backend-ale/api/editar_producto.php',
+  deleteUrl:   'https://TU-SUBDOMINIO.infinityfreeapp.com/backend-ale/api/eliminar_producto.php',
 
   // Ventas
-  saleUrl:     'https://abarrotesale.shop/backend-ale/api/guardar_venta.php'
+  saleUrl:     'https://abarrotesale.infinityfreeapp.com/backend-ale/api/guardar_venta.php'
 };

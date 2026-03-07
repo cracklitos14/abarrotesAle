8d28d1dfe6fd08e910c0c2b69cfb140446a258cb
Carlos S <sanchezvargascarlos1@gmail.com>
2026-03-07 14:18:51 -0600

    environments para railway1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,21 +1,7 @@
 export const environment = {
   production: true,
 
-  // Auth (login)
-  authUrl: '/api/login',
+  authUrl: 'https://backend-ale-production.up.railway.app/index.php?endpoint=login',
 
-  // API base
-  apiUrl: '/api/apis',
-
-  // Dashboard
-  dashboardUrl: 'https://abarrotesale.infinityfreeapp.com/backend-ale/api/dashboard.php',
-
-  // Productos
-  productsUrl: 'https://abarrotesale.infinityfreeapp.com/backend-ale/api/products.php',
-  createUrl:   'https://abarrotesale.infinityfreeapp.com/backend-ale/api/guardar_producto.php',
-  updateUrl:   'https://TU-SUBDOMINIO.infinityfreeapp.com/backend-ale/api/editar_producto.php',
-  deleteUrl:   'https://TU-SUBDOMINIO.infinityfreeapp.com/backend-ale/api/eliminar_producto.php',
-
-  // Ventas
-  saleUrl:     'https://abarrotesale.infinityfreeapp.com/backend-ale/api/guardar_venta.php'
+  apiUrl: 'https://backend-ale-production.up.railway.app/api'
 };
